3f9a743275e68c274dfe5d43c7188edcd07bf69f
hiddencilantro <stevekim1109@gmail.com>
2020-10-07 08:25:47 +0900

    Done.

--- prime.rb
+++ prime.rb
@@ -1,7 +1,9 @@
 def prime?(integer)
-    if integer > 1
-        num = (2..(integer-1)).to_a
-        num.all?{|n| integer%n != 0}
+    if integer == 2
+        true
+    elsif integer > 1
+        range = (2..(integer-1)).to_a
+        range.all?{|n| integer%n != 0}
     else
         false
     end
